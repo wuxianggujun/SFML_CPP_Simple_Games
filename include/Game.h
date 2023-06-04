9367c7602658086a890c9f223dadeb3b82e2c2ad
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2023-06-04 20:05:01 +0800

    Game1已经写好了，就是方块掉落点击获得积分，方块超出窗口生命值减少

--- include/Game.h
+++ include/Game.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <sstream>
 #include "SFML/Graphics.hpp"
 #include "SFML/System.hpp"
 #include "SFML/Window.hpp"
@@ -19,16 +23,52 @@ private:
     sf::VideoMode videoMode;
     sf::Event event;
 
-    void initVariables();
+    //Mouse positions
+    sf::Vector2i mousePosWindow;
+    sf::Vector2f mousePosView;
 
+    //Resources
+    sf::Font font;
+
+    sf::Text text;
+
+
+    //Game logic
+    bool endGame;
+    unsigned points;
+    int health;
+    float enemySpawnTimer;
+    float enemySpawnTimerMax;
+    int maxEnemies;
+    bool mouseHeld;
+
+    //Game Objects
+    std::vector<sf::RectangleShape> enemies;
+    sf::RectangleShape enemy;
+
+    void initVariables();
     void initWindow();
+    void initFonts();
+    void initText();
+    void initEnemies();
 
 public:
     Game();
     virtual ~Game();
+
     const bool running() const;
+    const bool getEndGame() const;
+
+    void spawnEnemy();
+
     void pollEvents();
+    void updateMousePositions();
+    void updateText();
+    void updateEnemies();
     void update();
+
+    void renderText(sf::RenderTarget& target);
+    void renderEnemies(sf::RenderTarget& target);
     void render();
 
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
 #include "include/Game.h"
 
 using namespace sf;
 
 int main() {
+
+    //Init srand
+   std::srand( static_cast<unsigned>(time(nullptr)));
 
     //Init Game engine
     Game game;
