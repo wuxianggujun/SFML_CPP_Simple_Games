0e000fd558f34fe1b91d5d0e5cf9841737e7fad5
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2023-06-12 22:11:26 +0800

    添加按钮

---
+++ SFML_RPG/include/Button.h
@@ -0,0 +1,14 @@
+//
+// Created by WuXiangGuJun on 2023/6/12.
+//
+
+#ifndef SFML_RPG_BUTTON_H
+#define SFML_RPG_BUTTON_H
+
+
+class Button {
+
+};
+
+
+#endif //SFML_RPG_BUTTON_H
---
+++ SFML_RPG/src/Button.cpp
@@ -0,0 +1,5 @@
+//
+// Created by WuXiangGuJun on 2023/6/12.
+//
+
+#include "include/Button.h"
